a5272fc3991fcbd122966f554191900558ef4a14
ncs <ncs19960831@163.com>
2019-10-20 07:15:43 -0700

    尝试打通SQLTable到主界面的通道。 因为暂时没有使用完全的SQL指令，所以暂时限制比较多，需要后期精简

--- datamanage.cpp
+++ datamanage.cpp
@@ -1,10 +1,84 @@
 #include "datamanage.h"
-#include "QSqlDatabase"
-#include "QSqlQuery"
+#include <QSqlDatabase>
+#include <QSqlTableModel>
+#include <QSqlTableModel>
+#include <QUuid>
+#include "dayreport.h"
 
-DataManage::DataManage()
+QSqlDatabase      thisSqlDataBase;
+QSqlTableModel*    thisSqlTableModel;
+
+
+
+bool DataManage::InitSqlDataBase(QWidget SqlTable,
+                                 QString Database,
+                                 QString DatabaseName,
+                                 QString HostName,
+                                 QString UserName,
+                                 QString PassWord,
+                                 int port)
 {
-    QSqlQuery SqlQuery;
-    QSqlDatabase MySqlDataBase = QSqlDatabase::addDatabase("DayReport");
+    if (!thisSqlDataBase.isOpen())
+    {
+        return true;
+    }
+    QSqlDatabase MySqlDataBase = QSqlDatabase::addDatabase(Database);
+    QSqlTableModel* retSqlTableModel = new QSqlTableModel(&SqlTable,MySqlDataBase);
+    MySqlDataBase.setDatabaseName(DatabaseName);
+
+    MySqlDataBase.setHostName(HostName);
+    MySqlDataBase.setUserName(UserName);
+    MySqlDataBase.setPassword(PassWord);
+    MySqlDataBase.setPort(20000);
+
+    if (MySqlDataBase.open() == true)
+    {
+        thisSqlDataBase = MySqlDataBase;
+        thisSqlTableModel = retSqlTableModel;
+        return true;
+    }
+}
+
+DataManage::
+
+void DataManage::DataManage(QWidget SqlTable)
+{
+
+    if (InitSqlDataBase(SqlTable,
+                    "DayReport",
+                    "DayReport.db",
+                    "NCSData",
+                    "ncs",
+                    "c^9%$27fiF274Dfg8@24!tcS8gvrr",
+                    20000))
+    {
+
+
+    }
+    else {
+        return;
+    }
+
+}
+
+void DataManage::SaveDataBase(QString String)
+{
+
+}
+
+void DataManage::SetOneData(DayReport DayReport)
+{
+    QString String;
+    QSqlTableModel SqlTableModel = thisSqlTableModel;
+    if (!SqlTableModel.isValid() )
+    {
+        return;
+    }
+    if (!DayReport.GetNumber(&String))
+    {
+        return;
+    }
+    DayReport.GetStartTime(&String);
+    QUuid *Uuid = new QUuid(String);
 
 }
